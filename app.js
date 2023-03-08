9f0587573e1c8c18a532b15da2aaeaf7d7945732
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-03-08 14:00:41 +0530

    Writing the message from the input text

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const http=require('http');
-
+const fs=require('fs');
 const server=http.createServer((req,res)=>{
-    console.log("Anurag");
-})
-server.listen(4000);
+    const url=req.url;
+    const method=req.method;
+    if(url=='/'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>My First App</title></head>');
+        res.write('<body><form action=/message method=POST><input type="text" name="message"><button>Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }else if(url=='/message' && method=='POST'){
+        const body=[];
+        req.on('data',(chunk)=>{
+            body.push(chunk);
+        });
+        return req.on('end',()=>{
+            const parseBody=Buffer.concat(body).toString();
+            const message=parseBody.split('=')[1];
+            fs.writeFile('message.txt',message,(err)=>{
+                res.statusCode=302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
+        });
+    }
+});
+server.listen(4000); 
